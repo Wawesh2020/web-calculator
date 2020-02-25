dac9ae7a839460a4c8a0f4bc1c73aa8c65744553
Wawesh2020 <henry@Henrys-MacBook-Air.local>
2020-02-25 16:34:35 -0500

    Created a Web Calculator with information from README using HTML, Javascript and CSS

---
+++ script.js
@@ -0,0 +1,12 @@
+
+<script>
+    function calc() {
+        var x = Number (document.getElementById)("first").value;
+        var y = Number (document.getElementById)("second").value;
+        var z = x+y;
+        document.getElementById("result").value=z;
+        
+    }
+        
+    
+</script>
