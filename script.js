dbdf2a82baeab294099e326294b5748ba32e13a4
Wawesh2020 <henry@Henrys-MacBook-Air.local>
2020-02-28 20:20:04 -0500

    Made changes to web-calculator

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function calculate(){
     var num1 = document.getElementById('num1').value
     var num2 = document.getElementById('num2').value
 
-    var answer = parseInt(num1) + parseInt(num2)
+    var answer = parseInt(num1) * parseInt(num2)
 
     document.getElementById('answer').innerHTML = answer
 
